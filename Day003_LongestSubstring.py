3b24cd2f59ef4e0cf903ae52485c18636ef2cac6
Deepak Jadhav <137857979+DeepxkJadhav@users.noreply.github.com>
2025-08-15 23:19:41 +0530

    Day003_LongestSubstring.py

--- Day003_LongestSubstring.py
+++ Day003_LongestSubstring.py
@@ -1,5 +1,7 @@
 Day 3 – Longest Substring Without Repeating Characters
 
+## Problem Statement
+Given a string, find the **length** of the longest substring without repeating characters.
 
 # Sliding Window Approach (O(n)) :
 
@@ -23,3 +25,10 @@ if __name__ == "__main__":
     print(length_of_longest_substring("abcabcbb"))  # 3 ("abc")
     print(length_of_longest_substring("bbbbb"))     # 1 ("b")
     print(length_of_longest_substring("pwwkew"))    # 3 ("wke")
+
+#Key Python Tricks 🐍
+- Using `set()` for O(1) duplicate checks.
+- Moving `left` pointer dynamically without restarting from scratch.
+- `max_len = max(max_len, right - left + 1)` to track window size on the fly.
+    
+Lesson Learned: Sliding window is the “Swiss Army knife” of string problems.
